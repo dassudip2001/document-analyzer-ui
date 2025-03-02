e1914f505a70e54038ee4f2cf6275387a16796b5
Sudip Das <sudip5428@gmail.com>
2025-03-02 20:24:53 +0530

    codr commit for enverment changes

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -1,15 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChatService {
-  API_URL = environment.apiUrl;
   constructor(private http: HttpClient) {}
 
   sendMessage(text: string) {
-    return this.http.post(`${this.API_URL}/analyze`, { text });
+    return this.http.post('/analyze', { text });
   }
 }
